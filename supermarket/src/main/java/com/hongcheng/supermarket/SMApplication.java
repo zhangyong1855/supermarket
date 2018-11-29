f43683c0054ce072d47018d96256206dcc1ef93d
Admin <Admin@windows10.microdone.cn>
2018-11-29 18:08:13 +0800

    时间:2018/11/29/18：07 版本:第一版 提交人：许博文

---
+++ supermarket/src/main/java/com/hongcheng/supermarket/SMApplication.java
@@ -0,0 +1,12 @@
+package com.hongcheng.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SMApplication.class, args);
+	}
+}
